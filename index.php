489497687496d9a06cc489ff2119fd52edc01fe1
renzotello <renzotj@gmail.com>
2016-02-08 20:03:39 +1000

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,14 @@
  * wp-blog-header.php which does and tells WordPress to load the theme.
  *
  * @package WordPress new datga
- */
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ ** Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
 
 /**
  * Tells WordPress to load the WordPress theme and output it.
